2a33ed4ecaf68d8c973a65c3e4906c6876231d51
zhangj52 <jichao.zhang@emc.com>
2021-05-02 01:15:38 -0700

    refine

--- storage_capacity_predict.py
+++ storage_capacity_predict.py
@@ -1,7 +1,18 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     df=pd.read_csv('data.csv', sep=',')
     vals = df.values
     print(type(vals))
     print(vals)
+    days = vals[:,0]
+    full_percent = vals[:, 1]
+    print(days)
+    print(full_percent)
+    plt.scatter(days, full_percent,  color='black')
+
+    #plt.xticks(())
+    #plt.yticks(())
+
+    plt.show()
