6e6f53b5da59990823a2f78f44362e504076bd8c
zhangj52 <jichao.zhang@emc.com>
2021-05-02 02:34:02 -0700

    refine

--- storage_capacity_predict.py
+++ storage_capacity_predict.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 
 # http://www.stat.yale.edu/Courses/1997-98/101/anovareg.htm
 # https://en.wikipedia.org/wiki/Coefficient_of_determination
@@ -11,12 +12,17 @@ def main():
     pass
 
 
+def caculate_r_square(y_true, y_predict):
+    return r2_score(y_true, y_predict)
+
 def get_boundary(days, full_percent):
     start_day = -10
 
     plt.scatter(days, full_percent,  color='red')
     # Train the model using the training sets
-    TODO = -20
+    TODO = -100
+    max_r2 = -100
+    max_idx = None
     for i in range(-10, TODO, -1):
         x = days[i:]
         y = full_percent[i:]
@@ -25,9 +31,19 @@ def get_boundary(days, full_percent):
         regr = linear_model.LinearRegression()
         regr.fit(x, y)
         preds = regr.predict(x)
+        r2 = caculate_r_square(y, preds)
+        if r2  > max_r2:
+            max_r2 = r2
+            max_idx = i
+            max_x = x
+            max_preds = preds
 
-        plt.plot(x, preds, color='blue', linewidth=3)
+        #plt.plot(x, preds, color='blue', linewidth=3)
+    print(max_r2)
+    print(max_idx)
+    plt.plot(max_x, max_preds, color='blue', linewidth=3)
     plt.show()
+
 
 
 
